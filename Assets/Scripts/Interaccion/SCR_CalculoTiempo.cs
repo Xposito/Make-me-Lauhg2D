e1d3ee60159bc470d2b4016fa5b27c3aed0f9a04
Xposito <114917289+Xposito@users.noreply.github.com>
2024-01-27 06:11:10 +0100

    Randomizar la aparición

--- Assets/Scripts/Interaccion/SCR_CalculoTiempo.cs
+++ Assets/Scripts/Interaccion/SCR_CalculoTiempo.cs
@@ -1,41 +1,141 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SCR_CalculoTiempo : MonoBehaviour
 {
-    private float currentTime = 0f;
-    private float targetTime = 120f; // Tiempo deseado de espera en segundos
-    private bool isWaiting = false;
+    SCO_SceneManager sceneManager;
+    public GameObject[] spawnpoints;
+    public GameObject lacayos_1;
+    public GameObject lacayos_2;
+    public GameObject lacayos_3;
+    public GameObject lacayos_4;
 
+    public GameObject beanLisa;
+    public GameObject beanRallada;
+    public GameObject beanPuntos;
+    public GameObject beanEstrellada;
+
+    public bool Iswaiting = false;
+
+    private float currentTime = 0f;
+    private float lacayosTime = 0f;
+    private float targetTime; // Tiempo deseado de espera en segundos
+    
+
+    private void Start()
+    {
+        sceneManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<SCR_Holder>().sceneManager;
+        targetTime = sceneManager.tiempoNivel;
+        StartCoroutine(CustomWait());
+        //Restart();
+    }
     void Update()
     {
         // Lógica personalizada de tiempo
         currentTime += Time.deltaTime;
+        lacayosTime += Time.deltaTime;
 
         // Comienza la espera cuando se presiona la tecla 'Espacio'
-        if (Input.GetKeyDown(KeyCode.Space) && !isWaiting)
+        if(lacayosTime >= sceneManager.tiempoSpawnLacayos)
         {
-            StartCoroutine(CustomWait());
+            ElegirLacayo();
         }
+        
+        
     }
 
     // Coroutine personalizada para la espera
     IEnumerator CustomWait()
     {
-        isWaiting = true;
-
+        
+        
         while (currentTime < targetTime)
         {
             // Puedes realizar acciones mientras esperas, si es necesario
             Debug.Log("Esperando...");
+            
 
             yield return null;
         }
+        
 
         // Restablece el tiempo y realiza acciones después de la espera
         currentTime = 0f;
-        isWaiting = false;
+        
         Debug.Log("Espera completada");
     }
+
+
+    void ElegirLacayo()
+    {
+        int randomlacayo = Random.Range(0, 100);
+
+        
+        if (randomlacayo < sceneManager.probabilidadLacayo1)
+        {
+            RandamoPosition(lacayos_1);
+        }
+        else if (sceneManager.probabilidadLacayo1 <= randomlacayo && randomlacayo < sceneManager.probabilidadLacayo2)
+        {
+            RandamoPosition(lacayos_2);
+        }
+        else if (sceneManager.probabilidadLacayo2 <= randomlacayo && randomlacayo < sceneManager.probabilidadLacayo3)
+        {
+            RandamoPosition(lacayos_3);
+        }
+        if (sceneManager.probabilidadLacayo3 <= randomlacayo && randomlacayo < sceneManager.probabilidadLacayo4)
+        {
+            RandamoPosition(lacayos_4);
+        }
+
+    }
+    void RandomBean(GameObject lacayo)
+    {
+        int ramdomBean = Random.Range(0, 100);
+
+        Transform handlerbean = lacayo.GetComponentInChildren<Transform>();
+        if(ramdomBean < sceneManager.probalidadSpawnbeanLisa)
+        {
+            Instantiate(beanLisa, handlerbean.transform.position, Quaternion.identity);
+        }
+        else if (sceneManager.probalidadSpawnbeanLisa <= ramdomBean && ramdomBean < sceneManager.probalidadSpawnbeanRallada)
+        {
+            Instantiate(beanRallada, handlerbean.transform.position, Quaternion.identity);
+        }
+        else if (sceneManager.probalidadSpawnbeanRallada <= ramdomBean && ramdomBean < sceneManager.probalidadSpawnbeanPuntos)
+        {
+            Instantiate(beanPuntos, handlerbean.transform.position, Quaternion.identity);
+        }
+        if (sceneManager.probalidadSpawnbeanPuntos <= ramdomBean && ramdomBean < sceneManager.probalidadSpawnbeanEstrellada)
+        {
+            Instantiate(beanEstrellada, handlerbean.transform.position, Quaternion.identity);
+        }
+
+    }
+
+    void RandamoPosition(GameObject lacayoRandom)
+    {
+        //StopCoroutine(SpawnLacayos());
+        //StartCoroutine(SpawnLacayos());
+        int randomNumber = Random.Range(0, 100);
+
+        Debug.Log(randomNumber);
+
+        if (randomNumber <= 50)
+        {
+            GameObject lacayoIntancia = Instantiate(lacayoRandom, spawnpoints[0].transform.position, Quaternion.identity); 
+            //Instantiate(lacayos_1, spawnpoints[0].transform.position, Quaternion.identity);
+            RandomBean(lacayoIntancia);
+        }
+        else
+        {
+            GameObject lacayoIntancia = Instantiate(lacayoRandom, spawnpoints[1].transform.position, Quaternion.identity);
+            //Instantiate(lacayos_1, spawnpoints[0].transform.position, Quaternion.identity);
+            RandomBean(lacayoIntancia);
+        }
+        lacayosTime = 0f;
+    }
+    
 }
--- Assets/Scripts/ScriptableObjects/SCO_SceneManager.cs
+++ Assets/Scripts/ScriptableObjects/SCO_SceneManager.cs
@@ -8,11 +8,11 @@ public class SCO_SceneManager : ScriptableObject
 {
 
     [Header("Slider")]
-
     public float timer;
     //Este numero NO puede ser menor 0 o menor.
     public float timerSpeed;
 
+    [Header("Requisitos")]
     public bool patito;
     public bool cajitaMusica;
     public bool confeti;
@@ -21,6 +21,21 @@ public class SCO_SceneManager : ScriptableObject
     public bool beanRallada;
     public bool beanPuntos;
     public bool beanEstrellas;
+
+    [Header("NivelCaracterísticas")]
+    public float tiempoNivel;
+    public float tiempoSpawnLacayos;
+    public float tiempoSpawnChistes;
+    public float probalidadSpawnbeanLisa;
+    public float probalidadSpawnbeanRallada;
+    public float probalidadSpawnbeanPuntos;
+    public float probalidadSpawnbeanEstrellada;
+    public float probabilidadLacayo1;
+    public float probabilidadLacayo2;
+    public float probabilidadLacayo3;
+    public float probabilidadLacayo4;
+
+
     
     
 
